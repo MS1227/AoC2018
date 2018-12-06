0832939bfa55819b52a7ae4bafb5e35c2fac8afd
Matt Schnider <matt.schnider@gmail.com>
2018-12-06 13:55:50 -0500

    got the answer

--- day1.java
+++ day1.java
@@ -5,16 +5,20 @@ public class day1
 {
     public static void main(String [] args) throws FileNotFoundException
     {
+       int result = 0; 
+
+
        if(args.length > 0)
        {
            String filename = new String(args[0]);
 
            Scanner infile = new Scanner(new File(filename));
-
            while(infile.hasNext())
            {
-                System.out.println(infile.nextLine());
+                
+                result += infile.nextInt();
            }
+           System.out.println("Answer = " + result);
        }
        else
        {
